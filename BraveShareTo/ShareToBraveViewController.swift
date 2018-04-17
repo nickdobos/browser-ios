7a7ba845bf6f96cde5bf47e4741623fb3e6e0039
Michał Buczek <michal.buczek@protonmail.com>
2018-04-17 17:00:51 +0200

    Fix #438: allow to open in Brave more url types from other apps. (#1485)

--- BraveShareTo/ShareToBraveViewController.swift
+++ BraveShareTo/ShareToBraveViewController.swift
@@ -1,54 +1,72 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import UIKit
 import Social
 import MobileCoreServices
 
-extension NSObject {
-    func callSelector(_ selector: Selector, object: AnyObject?, delay: TimeInterval) {
-        let delay = delay * Double(NSEC_PER_SEC)
-        let time = DispatchTime.now() + Double(Int64(delay)) / Double(NSEC_PER_SEC)
-        DispatchQueue.main.asyncAfter(deadline: time, execute: {
-            Thread.detachNewThreadSelector(selector, toTarget:self, with: object)
-        })
-    }
-}
-
 class ShareToBraveViewController: SLComposeServiceViewController {
 
-    override func isContentValid() -> Bool {
-        return true
-    }
-
-    override func didSelectPost() {
-        return
+    // TODO: Separate scheme for debug builds, so it can be tested without need to uninstall production app.
+    private func urlScheme(for url: String) -> NSURL? {
+        return NSURL(string: "brave://open-url?url=\(url)")
     }
 
     override func configurationItems() -> [Any]! {
-        let item: NSExtensionItem = extensionContext!.inputItems[0] as! NSExtensionItem
-        let itemProvider: NSItemProvider = item.attachments![0] as! NSItemProvider
-        let type = kUTTypeURL as String
+        guard let inputItems = extensionContext?.inputItems as? [NSExtensionItem], let attachment = inputItems.first?.attachments?.first as? NSItemProvider else {
+            return []
+        }
 
-        if itemProvider.hasItemConformingToTypeIdentifier(type) {
-            itemProvider.loadItem(forTypeIdentifier: type, options: nil, completionHandler: {
-                (urlItem, error) in
-                guard let url = (urlItem as! URL).absoluteString.addingPercentEncoding(withAllowedCharacters: CharacterSet.alphanumerics),
-                    let braveUrl = URL(string: "brave://open-url?url=\(url)") else { return }
+        var itemProvider: NSItemProvider?
+        // Look for the first URL the host application is sharing.
+        // If there isn't a URL grab the first text item
+        if attachment.isUrl || attachment.isText {
+            itemProvider = attachment
+        }
 
-                // From http://stackoverflow.com/questions/24297273/openurl-not-work-in-action-extension
-                var responder = self as UIResponder?
-                while (responder != nil) {
-                    if responder!.responds(to: #selector(UIApplication.openURL(_:))) {
-                        responder!.callSelector(#selector(UIApplication.openURL(_:)), object: braveUrl as AnyObject, delay: 0)
-                    }
-                    responder = responder!.next
-                }
+        guard let provider = itemProvider else {
+            // If no item was processed. Cancel the share action to prevent the extension from locking the host application
+            // due to the hidden ViewController.
+            cancel()
+            return []
+        }
 
-                DispatchQueue.main.asyncAfter(
-                    deadline: DispatchTime.now() + Double(Int64(0.1 * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC), execute: { self.cancel() })
-            })
+        provider.loadItem(of: provider.isUrl ? kUTTypeURL : kUTTypeText) { item, error in
+            var urlItem: NSURL!
+
+            // We can get urls from other apps as a kUTTypeText type, for example from Apple's mail.app.
+            if let text = item as? String {
+                urlItem = NSURL(string: text)
+            } else if let url = item as? NSURL {
+                urlItem = url
+            } else {
+                self.cancel()
+                return
+            }
+
+            if let braveUrl = urlItem.encodedUrl.flatMap(self.urlScheme) {
+                self.handleUrl(braveUrl)
+            }
         }
 
         return []
+    }
+
+    private func handleUrl(_ url: NSURL) {
+        // From http://stackoverflow.com/questions/24297273/openurl-not-work-in-action-extension
+        var responder = self as UIResponder?
+        while let strongResponder = responder {
+            let selector = #selector(UIApplication.openURL(_:))
+            if strongResponder.responds(to: selector) {
+                strongResponder.callSelector(selector, object: url, delay: 0)
+            }
+            responder = strongResponder.next
+        }
+
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime(uptimeNanoseconds: UInt64(0.1 * Double(NSEC_PER_SEC)))) {
+            self.cancel()
+        }
     }
 
     override func viewDidAppear(_ animated: Bool) {
@@ -61,5 +79,28 @@ class ShareToBraveViewController: SLComposeServiceViewController {
 
     override func willMove(toParentViewController parent: UIViewController?) {
         view.alpha = 0
+    }
+}
+
+extension NSItemProvider {
+    var isText: Bool { return hasItemConformingToTypeIdentifier(String(kUTTypeText)) }
+    var isUrl: Bool { return hasItemConformingToTypeIdentifier(String(kUTTypeURL)) }
+
+    func loadItem(of type: CFString, completion: CompletionHandler?) {
+        loadItem(forTypeIdentifier: String(type), options: nil, completionHandler: completion)
+    }
+}
+
+extension NSURL {
+    var encodedUrl: String? { return absoluteString?.addingPercentEncoding(withAllowedCharacters:NSCharacterSet.alphanumerics) }
+}
+
+extension NSObject {
+    func callSelector(_ selector: Selector, object: AnyObject?, delay: TimeInterval) {
+        let delay = delay * Double(NSEC_PER_SEC)
+        let time = DispatchTime.now() + Double(Int64(delay)) / Double(NSEC_PER_SEC)
+        DispatchQueue.main.asyncAfter(deadline: time) {
+            Thread.detachNewThreadSelector(selector, toTarget:self, with: object)
+        }
     }
 }
